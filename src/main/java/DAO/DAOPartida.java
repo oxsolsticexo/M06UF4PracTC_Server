750a7ecb64ead3642dcd973507e034ec3807580f
Carlos Jimenez <jimenez.martin.carlos@alumnat.copernic.cat>
2023-05-25 18:40:47 +0200

    añadir datos partidas

---
+++ src/main/java/DAO/DAOPartida.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+/**
+ *
+ * @author Carlos
+ */
+public class DAOPartida {
+    
+    public DAOPartida(){
+        
+    }
+    
+    public void crearPartida(String nombre,String dificultad){
+        
+    }
+}
---
+++ src/main/java/Interfaces/LogicaPartidaInterface.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author Carlos
+ */
+public interface LogicaPartidaInterface {
+    void crearPartida();
+    
+}
